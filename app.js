52c64d567053ac31734672d185504e689a8f7b81
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2022-03-21 22:39:31 +0530

    Added css to it and do some changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 let items = ["Buy Food", "Cook Food" , "Eat Food"];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); 
 
 app.get("/", function (req, res) {
   let today = new Date();
