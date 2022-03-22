96575b17b05a1977eecd7e01f76a3073bc0c6485
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2022-03-22 22:11:58 +0530

    Added work page using ejs and js

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-let items = ["Buy Food", "Cook Food" , "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+let tdnItems = [];
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -22,13 +24,42 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day , newListItems: items});
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-   let item = req.body.newItem;
-   items.push(item);
-  res.redirect("/");
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else if (req.body.list === "Tdn") {
+    workItems.push(item);
+    res.redirect("/tdn");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/tdn", function (req, res) {
+  res.render("list", { listTitle: "TDNOW List", newListItems: tdnItems });
+});
+
+app.post("/tdn", function (req, res) {
+  let item = req.body.newItem;
+  tdnItems.push(item);
+  res.redirect("/tdn");
 });
 
 app.listen(3000, function () {
